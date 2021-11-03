79e3707b5830658a2a99176d094e3c90748f5e4a
shankarlmc <shankarlmc012@gmail.com>
2021-11-03 10:32:06 +0545

    4th sem completed

--- 4th_sem/Practical/Os_Practical/sstf-disk-scheduling.cpp
+++ 4th_sem/Practical/Os_Practical/sstf-disk-scheduling.cpp
@@ -4,7 +4,7 @@ sstf disk algorithm
 Example: 
 	Number of Location: 8
 	Position Of Head: 53
-	Enter the elements of dis queue: 98, 183, 122, 14,122, 124, 65, 67 
+	Enter the elements of dis queue: 98, 183, 37, 122, 14, 124, 65, 67 
 */
 #include<stdio.h>
 #include<stdlib.h>
@@ -19,20 +19,21 @@ int main(){
 	struct di d[50];
 	int arr[50], a[50];
 	
-	printf("Enter the number of location: \n");
-	scanf("%d", &loc);
-	printf("Enter the Position of head: \n");
+	printf("Enter the number of location: ");
+	scanf("%d", &n);
+	printf("Enter the Position of head: ");
 	scanf("%d", &head);
 	printf("Enter the elements of dis queue: \n");
-	for(i=0; i<n; i++){
+	for(i=0; i < n; i++){
+		printf("Element No. %d = ", i);
 		scanf("%d", &d[i].num);
 		d[i].flag=0;
 	}
-	for(i=0;i<n;i++){
+	for(i = 0; i < n; i++){
 		x = 0;
 		min = 0;
 		loc = 0;
-		for(j=0;j<n;j++){
+		for(j = 0; j < n; j++){
 			if(d[j].flag == 0){
 				if(x==0){
 					arr[j] = head-d[j].num;
@@ -63,7 +64,7 @@ int main(){
 		
 	}
 	// Finding the seek time	
-	for(i=0; i<n;i++){
+	for(i=0; i < n;i++){
 		seek = seek + a[i];	
 	}
 	printf("\nTotal head movement is %d", seek);
